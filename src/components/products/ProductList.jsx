145aef3eca4e65f9b76efb4db248cc57e1ac881c
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-08 17:29:58 +0100

    product list axios

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function ProductList() {
+
+  // 'https://dummyjson.com/products'
+
+const [mainProductsArr, setmainProductsArr] = useState([])
+
+useEffect(() => {
+  axios.get('https://dummyjson.com/products')
+  .then((resp) => {
+    console.log('resp ===', resp)})
+  .catch(error => {
+  console.warn('ivyko klaida:', error);
+  })
+}, [])
+
+
   return (
-    <div>ProductList</div>
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
   )
 }
 
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ProductList from '../components/products/ProductList'
 
 export default function ProductPage() {
   return (
     <div className='container'>
       <h1 className='text-3xl mb-5'>ProductPage</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias quia eligendi error repellendus? Tenetur commodi saepe voluptas ad, inventore consectetur!</p>
+      <ProductList />
     </div>
   )
 }

cceab20fbc77b2141480327c5a36d18d1c01ef27
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-08 17:50:08 +0100

    get add map items

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,16 +1,19 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
+const singleProductsObj = {"id":1,"title":"iPhone 9","description":"An apple mobile which is nothing like apple","price":549,"discountPercentage":12.96,"rating":4.69,"stock":94,"brand":"Apple","category":"smartphones","thumbnail":"https://cdn.dummyjson.com/product-images/1/thumbnail.jpg","images":["https://cdn.dummyjson.com/product-images/1/1.jpg","https://cdn.dummyjson.com/product-images/1/2.jpg","https://cdn.dummyjson.com/product-images/1/3.jpg","https://cdn.dummyjson.com/product-images/1/4.jpg","https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"]}
+
 function ProductList() {
 
   // 'https://dummyjson.com/products'
 
-const [mainProductsArr, setmainProductsArr] = useState([])
-
+const [mainProductsArr, setMainProductsArr] = useState([])
 useEffect(() => {
   axios.get('https://dummyjson.com/products')
   .then((resp) => {
-    console.log('resp ===', resp)})
+  const prodArr = resp.data.products
+  setMainProductsArr(prodArr)
+  })
   .catch(error => {
   console.warn('ivyko klaida:', error);
   })
@@ -20,9 +23,8 @@ useEffect(() => {
   return (
     <div>
       <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
+        {mainProductsArr.map((pObj) => <li key={pObj.id}>{pObj.title}</li>)}
+        
       </ul>
     </div>
   )
